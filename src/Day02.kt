fba60884878e858e755215705bf611bea9b1ffe4
Christian Birmes <christian.birmes@cologne-intelligence.de>
2021-12-02 09:43:10 +0100

    day 2 completed

---
+++ src/Day02.kt
@@ -0,0 +1,54 @@
+fun main() {
+
+    data class Command(val direction: String, val units: Int)
+
+    data class Position(val horizontal: Int, val depth: Int) {
+        fun executeCommand(command: Command) =
+            when (command.direction.lowercase()) {
+                "forward" -> copy(horizontal = horizontal + command.units)
+                "down" -> copy(depth = depth + command.units)
+                "up" -> copy(depth = depth - command.units)
+                else -> this
+            }
+    }
+
+    fun Iterable<String>.toCommands() =
+        map { it.split(' ', limit = 2) }
+            .filter { it.size >= 2 }
+            .filter { it[1].toIntOrNull() != null }
+            .map { Command(it[0], it[1].toInt()) }
+
+    fun part1(input: List<String>) = input
+        .toCommands()
+        .fold(Position(0, 0), Position::executeCommand)
+        .let { it.horizontal * it.depth }
+
+
+    data class PositionWithAim(val horizontal: Int, val depth: Int, val aim: Int) {
+        fun executeCommand(command: Command) =
+            when (command.direction.lowercase()) {
+                "forward" -> copy(
+                    horizontal = horizontal + command.units,
+                    depth = depth + aim * command.units
+                )
+                "down" -> copy(aim = aim + command.units)
+                "up" -> copy(aim = aim - command.units)
+                else -> this
+            }
+    }
+
+    fun part2(input: List<String>) = input
+        .toCommands()
+        .fold(PositionWithAim(0, 0, 0), PositionWithAim::executeCommand)
+        .let { it.horizontal * it.depth }
+
+
+    // test if implementation meets criteria from the description, like:
+    val testInput = readInput("Day02_test")
+    check(part1(testInput) == 150)
+    check(part2(testInput) == 900)
+
+    val input = readInput("Day02")
+    println(part1(input))
+    println(part2(input))
+}
